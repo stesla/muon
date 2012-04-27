2ba887871ef5450597b2caae113009a587255790
Samuel Tesla <samuel.tesla@gmail.com>
2012-04-26 23:15:47 -0500

    send output from MU line-at-a-time and ensure UTF-8 encoding

--- connect.go
+++ connect.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"code.google.com/p/go.net/websocket"
 	"github.com/stesla/gotelnet"
-	"io"
+	"log"
 	"net/http"
+	"unicode/utf8"
 )
 
 func ConnectServer(w http.ResponseWriter, r *http.Request) {
@@ -22,14 +24,40 @@ func ConnectServer(w http.ResponseWriter, r *http.Request) {
 func forward(down *websocket.Conn, up gotelnet.Conn) {
 	lineEnding := "\n";
 	exit := make(chan bool)
-	go func() { io.Copy(down, up); exit <- true }()
+	go func() {
+		b := bufio.NewReader(up)
+		for {
+			line, _, err := b.ReadLine()
+			if err != nil {
+				log.Println("Error: ReadLine:", err)
+				break
+			}
+			runes := []rune{}
+			for len(line) > 0 {
+				r, n := utf8.DecodeRune(line)
+				if r == utf8.RuneError {
+					r = '?'
+				}
+				runes = append(runes, r)
+				line = line[n:]
+			}
+			werr := websocket.Message.Send(down, string(runes))
+			if werr != nil {
+				log.Println("Error: Send:", werr)
+				break
+			}
+		}
+		exit <- true
+	}()
 	go func() {
 		for {
 			var msg string
 			if rerr := websocket.Message.Receive(down, &msg); rerr != nil {
+				log.Println("Error: Receive:", rerr);
 				break
 			}
 			if _, werr := up.Write([]byte(msg + lineEnding)); werr != nil {
+				log.Println("Error: Write:", werr);
 				break
 			}
 		}
