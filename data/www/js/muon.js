491c5ad3a37ca78110e54b6ee30ae6204f1d1b82
Samuel Tesla <stesla@drw.com>
2013-11-13 22:16:20 -0600

    refactored javascript for clarity

--- data/www/js/muon.js
+++ data/www/js/muon.js
@@ -1,5 +1,5 @@
 (function(window) {
-    var input_, output_, ws_;
+    var ws_;
 
     function connect(fn) {
         //TODO: wss when we're https
@@ -25,19 +25,30 @@
         }
     }
 
-    window.addEventListener('load', function() {
+    function setTextAreaWidthInCharacters(width) {
+        /* Presently, the only
+         * reliable way to do that is to actually get the width of one
+         * character as rendered on the screen because none of the CSS
+         * units work right. */
         var oneChar = document.getElementById('oneChar');
         var sheets = document.styleSheets;
         var sheet = sheets[sheets.length - 1];
-        sheet.insertRule('#container { width: ' + 80 * oneChar.clientWidth + 'px }', 0);
-        input_ = document.querySelector('#container .input textarea');
-        output_ = document.querySelector('#container .output');
-        input_.addEventListener('keydown', processInput);
+        sheet.insertRule('#container { width: ' + width * oneChar.clientWidth + 'px }', 0);
+    }
+
+    window.addEventListener('load', function() {
+        var input = document.querySelector('#container .input textarea');
+        var output = document.querySelector('#container .output');
+
+        setTextAreaWidthInCharacters(80);
+
+        input.addEventListener('keydown', processInput);
+
         ws_ = connect(function(text) {
             var line = document.createElement('p');
             line.innerHTML = text;
-            output_.appendChild(line);
-            input_.scrollIntoView();
+            output.appendChild(line);
+            input.scrollIntoView();
         });
     });
 })(window);
